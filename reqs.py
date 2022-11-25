43335d38e2ba6e03cdcc8ffc6ee1f8eb5f963758
mvterra <mvterra6@hotmail.com>
2022-11-25 09:11:00 -0300

    The project is finished

--- reqs.py
+++ reqs.py
@@ -3,14 +3,38 @@ import json
 
 base_url = "http://e00m.duckdns.org/"
 user_endpoint = "users/"
-
+user_vaccines_endpoint = "users/doses/"
+all_vaccines_endpoint = "vacs/"
 
 def get_user_info(username: str, password: str) -> requests.Response:
     """Makes a request to the server using the username and password
-    provided by the user and returns the response"""
+    provided by the user and returns the user info"""
 
     login_response = requests.get(base_url + user_endpoint, auth=(username, password))
     return login_response
+
+
+def get_user_vaccines(username: str, password: str) -> requests.Response:
+    """Makes a request to the server using the username and password
+    provided by the user and returns the user vaccines"""
+
+    login_response = requests.get(base_url + user_vaccines_endpoint, auth=(username, password))
+    return login_response
+
+
+def get_all_vaccines() -> requests.Response:
+    """Makes a request to the server using the username and password
+    provided by the user and returns vaccines available"""
+
+    login_response = requests.get(base_url + all_vaccines_endpoint)
+    return login_response
+
+
+def register_vac(user_info: tuple[str, str], vac_info: dict[str, str]) -> requests.Response:
+    """Register the vaccine and its date according to the user input"""
+    data = json.dumps(vac_info)
+    register_vac_response = requests.post(base_url + user_vaccines_endpoint, auth=user_info, data=data)
+    return register_vac_response
 
 
 def register_user(user_info: dict[str, str]) -> requests.Response:
